e8d40bcd67f2ff1b2cb98b5db13114756c08646a
ToxicMilk <qiujiangtao130@outlook.com>
2020-03-11 21:31:49 +0800

    added demo4

--- day13/src/cn/Demo3.java
+++ day13/src/cn/Demo3.java
@@ -1,7 +1,20 @@
 package cn;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+@WebServlet("/demo3")
 public class Demo3 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("doget");
+    }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("dopost");
+    }
 }
